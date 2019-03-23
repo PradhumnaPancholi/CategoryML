e8d7210809bb080e9f5b8c57837d0711afcb2d0a
Rushi Ray <pnpancholi6629.pnp@gmail.com>
2019-03-23 03:52:10 -0500

    vgcrfxed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const brain = require('brain.js')
-const data = require('./data.json')
+//const data = require('./data.json')
 //for network//
 const network = new brain.recurrent.LSTM()
 //function to prepare data to train model//
