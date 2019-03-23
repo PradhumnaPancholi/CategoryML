0985a369a351901d1be45a3ac3505bf65e8618ab
Rushi Ray <pnpancholi6629.pnp@gmail.com>
2019-03-23 03:43:26 -0500

    initial stage of app

---
+++ index.js
@@ -0,0 +1,16 @@
+const brain = require('brain.js')
+const data = require('./data.json')
+//for network//
+const network = new brain.recurrent.LSTM()
+//function to prepare data to train model//
+let mockData = data.map((que) => ({
+    input: que.question,
+    output: que.category
+}))
+//to train model with data//
+network.train(mockData, {
+    iterations: 200
+})
+
+let attempt = network.run("illustrator keeps crashing")
+console.log(`Category: ${attempt}`)
